6d15a53f7db5680752f8d7e9856ce7e8d6c4cb6c
Arthurpers <arthur.pers@ensta-bretagne.org>
2024-01-22 09:43:51 +0100

    fix q2 and results

--- q2.py
+++ q2.py
@@ -10,6 +10,7 @@ througpouts = []
 for file_size in file_sizes :
   for block_size in block_sizes :
     commande = "./q1 " + str(file_size) + " " + str(block_size)
+    print(commande)
     resultat = subprocess.run(commande, shell=True, capture_output=True, text=True)
     resultat = resultat.stdout.split("\n")
     # print(resultat)
@@ -18,30 +19,44 @@ for file_size in file_sizes :
 
 
 
-fig, ax = plt.subplots()
+# Create a figure and two subplots
+fig, (ax1, ax2) = plt.subplots(2)
 
-ax.plot(block_sizes, times[0:9], 'o')
+# Plot block sizes vs times on the first subplot
+ax1.plot(block_sizes, times[0:10], 'o') #Temps pour le fichier de 100 Mo 
+ax1.set_xlabel('Block Size (Ko)')
+ax1.set_ylabel('Time (s)')
+ax1.set_title('100 Mo file write ')
 
-# Set the labels for the x and y axes
-ax.set_xlabel('Block Size (Ko)')
-ax.set_ylabel('Time (s)')
+# Plot block sizes vs throughputs on the second subplot
+ax2.plot(block_sizes, times[10:20], 'o') #Temps pour le fichier de 1 Go
+ax2.set_xlabel('Block Size (Ko)')
+ax2.set_ylabel('Time (s)')
+ax2.set_title('1 Go file write')
 
-# Set the title of the plot
-ax.set_title('100 Mo file random write ')
+# Adjust the layout so that plots do not overlap
+plt.tight_layout()
 
-# Display the plot
-plt.savefig("plot.png")
+# Save the figure to a file
+plt.savefig("plot1-v2.png")
 
+# Create a figure and two subplots
+fig, (ax1, ax2) = plt.subplots(2)
 
-fig, ax = plt.subplots()
-ax.plot(block_sizes, times[10:19], 'o')
+# Plot block sizes vs times on the first subplot
+ax1.plot(block_sizes, througpouts[0:10], 'o')
+ax1.set_xlabel('Block Size (Ko)')
+ax1.set_ylabel('Throughput (Mo/s)')
+ax1.set_title('100 Mo file write ')
 
-# Set the labels for the x and y axes
-ax.set_xlabel('Block Size (Ko)')
-ax.set_ylabel('Time (s)')
+# Plot block sizes vs throughputs on the second subplot
+ax2.plot(block_sizes, througpouts[10:20], 'o')
+ax2.set_xlabel('Block Size (Ko)')
+ax2.set_ylabel('Throughput (Mo/s)')
+ax2.set_title('1 Go file write ')
 
-# Set the title of the plot
-ax.set_title('1 Go file random write')
+# Adjust the layout so that plots do not overlap
+plt.tight_layout()
 
-# Display the plot
-plt.savefig("plot.png")
+# Save the figure to a file
+plt.savefig("plot2-v2.png")
